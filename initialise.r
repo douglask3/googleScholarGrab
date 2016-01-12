3c788e49bfc2dc83e5d501f2821120714702ef57
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-12 10:51:26 +0000

    adding CRAN path to install packages so no erropr in command line

--- initialise.r
+++ initialise.r
@@ -1,4 +1,6 @@
-install.packages('devtools')
+#!/usr/bin/env r
+
+install.packages('devtools', repos = 'http://www.stats.bris.ac.uk/R/')
 library(devtools)
 install_bitbucket('douglask3/gitprojectextras/gitBasedProjects')#
-install.packages('scholar')
+install.packages('scholar', repos = 'http://www.stats.bris.ac.uk/R/')
