874cd940a223f7a2491de0db1dd78406dff4b175
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-12 11:51:18 +0000

    tidying

--- initialise.r
+++ initialise.r
@@ -2,5 +2,5 @@
 
 install.packages('devtools', repos = 'http://www.stats.bris.ac.uk/R/')
 library(devtools)
-install_bitbucket('douglask3/gitprojectextras/gitBasedProjects')#
+install_bitbucket('douglask3/gitprojectextras/gitBasedProjects')
 install.packages('scholar', repos = 'http://www.stats.bris.ac.uk/R/')
