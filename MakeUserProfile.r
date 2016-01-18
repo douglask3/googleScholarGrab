019b6056839b15875049b4ad10d064b7c0e7a02a
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-18 15:41:23 +0000

    two new format options for author profile

--- MakeUserProfile.r
+++ MakeUserProfile.r
@@ -12,11 +12,20 @@ MakeUserProfile <- function(usr, format = '1line') {
         profile = c('**Total Citations:**', profile$total_cites,'<br>',
                     '**H index:**', profile$h_index, '<br>',
                     '**i10 index:**', profile$i10_index)
+    } else if (format == 'bullets') {
+        profile = c(  '* Total Citations: ', profile$total_cites,
+                    '\n* H index: '        , profile$h_index,
+                    '\n* i10 index: '       , profile$i10_index)
+    } else if (format == 'unformatted') {
+        profile = c('Total Citations:  ', profile$total_cites,
+                    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;H index:  '        , profile$h_index,
+                    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;i10 index:  '       , profile$i10_index)
     }  else {
         profile = c('**Total Citations:**', profile$total_cites,';',
                     '**H index:**', profile$h_index, ';',
                     '**i10 index:**', profile$i10_index)
     }
     profile = paste(profile, collapse = ' ')
+    print(profile)
     return(profile)
 }
